01ede4138e92baa0803d66665f6d4de2f4f4c1a3
张博 <nobodylesszb@163.com>
2019-06-12 11:16:22 +0800

    增加pprint的练习

---
+++ data_structures/pprint/pprint_pformat.py
@@ -0,0 +1,33 @@
+#要格式化数据结构而不将其直接写入流（例如，用于记录），请使用pformat()构建字符串表示形式
+
+import logging
+from pprint import pformat
+from pprint_data import data
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)-8s %(message)s',
+)
+
+logging.debug('Logging pformatted data')
+formatted = pformat(data)
+for line in formatted.splitlines():
+    logging.debug(line.rstrip())
+
+"""
+DEBUG    Logging pformatted data
+DEBUG    [(1, {'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D'}),
+DEBUG     (2,
+DEBUG      {'e': 'E',
+DEBUG       'f': 'F',
+DEBUG       'g': 'G',
+DEBUG       'h': 'H',
+DEBUG       'i': 'I',
+DEBUG       'j': 'J',
+DEBUG       'k': 'K',
+DEBUG       'l': 'L'}),
+DEBUG     (3, ['m', 'n']),
+DEBUG     (4, ['o', 'p', 'q']),
+DEBUG     (5, ['r', 's', 'tu', 'v', 'x', 'y', 'z'])]
+
+"""
